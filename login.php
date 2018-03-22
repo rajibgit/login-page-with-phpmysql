27b07c1e6051afde88a6a89abe3cdff31da3254d
Md Rajib Bhuiyan <36604932+rajibgit@users.noreply.github.com>
2018-03-23 01:29:08 +0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>log in</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<img src="img\man.png"/>
+		<form>
+			<div class="form_input">
+				<input type="text" name="username" placeholder="Enter your name">
+			</div>
+
+			<div class="form_input">
+				<input type="text" name="password" placeholder="Enter valid password">
+			</div>
+			<input type="submit" name="submit" value="LOGIN" class="btn-login"/>
+
+		</form>
+	
+</body>
+</html>
